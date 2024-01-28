186842a9614accfb7dda42ffb95be77ccd328d6d
Waszabii1 <148143917+Waszabii1@users.noreply.github.com>
2024-01-28 18:54:39 +0000

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,3 @@
-theBoard = {"7": "7", "8": "8", "9": "9",
-            "4": "4", "5": "5", "6": "6",
-            "1": "1", "2": "2", "3": "3",
-}
-
-Board_keys = []
-
-for key in theBoard:
-    Board_keys.append(key)
-    
 def printBoard(board):
     print(board["7"] + "|" + board["8"] + "|" + board["9"])
     print("-+-+-")
@@ -18,14 +8,26 @@ def printBoard(board):
 def instructions():
     print("The grid is 1-9, from left to right with 1-3 as the bottom row")
     print("When indicated to do so, put a number between 1-9")
+    print("Type ""stop"" at any point to stop")
     
 
-def won(player):
+def won(player, theBoard):
     printBoard(theBoard)
     print("\nGame Over.\n")
     print(f"!!!! {player} won !!!!".upper())
     
 def game():
+    
+    theBoard = {"7": "⁷", "8": "⁸", "9": "⁹",
+            "4": "⁴", "5": "⁵", "6": "⁶",
+            "1": "¹", "2": "²", "3": "³",
+}
+
+
+    Board_keys = []
+
+    for key in theBoard.keys():
+        Board_keys.append(key)
     
     player = "X"
     count = 0
@@ -34,7 +36,10 @@ def game():
         printBoard(theBoard)
         print(f"It's your turn {player}. Where do you want to go?")
         
-        move = input()
+        move = input().upper()
+        
+        if move == "STOP":
+            break
         
         if move.isdigit():
             if theBoard[move] != "X" or "O":
@@ -48,29 +53,30 @@ def game():
         
         if count >= 5:
             if theBoard["7"] == theBoard["8"] == theBoard["9"]:
-                won(player)
+                won(player, theBoard)
                 break
             elif theBoard["4"] == theBoard["5"] == theBoard["6"]:
-                won(player)
+                won(player, theBoard)
                 break
             elif theBoard["1"] == theBoard["2"] == theBoard["3"]:
-                won(player)
+                won(player, theBoard)
                 break
             elif theBoard["1"] == theBoard["4"] == theBoard["7"]:
-                won(player)
+                won(player, theBoard)
                 break
             elif theBoard["2"] == theBoard["5"] == theBoard["8"]:
-                won(player)
+                won(player, theBoard)
                 break
             elif theBoard["3"] == theBoard["6"] == theBoard["9"]:
-                won(player)
+                won(player, theBoard)
                 break
             elif theBoard["1"] == theBoard["5"] == theBoard["9"]:
-                won(player)
+                won(player, theBoard)
                 break
             elif theBoard["7"] == theBoard["5"] == theBoard["3"]:
-                won(player)
+                won(player, theBoard)
                 break
+            
         if count == 9:
             printBoard(theBoard)
             print("\nGame Over.\n")
@@ -83,10 +89,8 @@ def game():
             player = "X"
         
     restart = input("Do you want to play again? (Y/N) ").upper()
+    
     if restart == "Y":
-        for key in Board_keys:
-            theBoard[key] = " "
-                
         game()
 
 if __name__ == "__main__":
